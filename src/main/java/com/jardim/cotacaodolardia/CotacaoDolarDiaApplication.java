c65b3725d6e08b0f1c6f99e0683da1591a6229b7
YuriCJardim <yuricjardim@gmail.com>
2022-12-22 12:12:01 -0300

    feat: criando projeto

---
+++ src/main/java/com/jardim/cotacaodolardia/CotacaoDolarDiaApplication.java
@@ -0,0 +1,13 @@
+package com.jardim.cotacaodolardia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacaoDolarDiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotacaoDolarDiaApplication.class, args);
+	}
+
+}
